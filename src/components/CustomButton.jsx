b456aecff036e2cc82362a95131eda270eaa362d
rajanhere <rajan@brainstackhub.com.au>
2024-01-21 20:17:53 +1100

    first commit

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function CustomButton({ label, cls, clickHandler }) {
+  return (
+    <div className={cls} onClick={() => clickHandler(label)}>
+      {label}
+    </div>
+  );
+}
+CustomButton.propTypes = {
+  label: PropTypes.string,
+  cls: PropTypes.string,
+  clickHandler: PropTypes.func,
+};
+export default CustomButton;
